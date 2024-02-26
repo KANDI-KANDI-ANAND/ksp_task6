fcfb65732ce4c581b3836801575f261a65addf16
KANDI KANDI ANAND <160637109+KANDI-KANDI-ANAND@users.noreply.github.com>
2024-02-27 01:09:22 +0530

    Add files via upload

---
+++ last/basic.py
@@ -0,0 +1,65 @@
+print("hello world")
+number1 = 8
+print(number1)
+
+#to know what datatype it is
+print(type(number1))
+name = "Laddu"
+print(type(name))
+
+#one data type into another
+number2 = 4
+number3 = 5.6
+name1 = "anand"
+print(int(number3))
+print(str(number2))
+
+
+#strings
+name2 = "jsehgsdfsuirrgfshd"
+print(len(name2))
+
+#indexes
+hope = "laddulovesmaari"
+print(hope[0])
+print(hope[0:5])
+print(hope[5:])
+
+
+names = "lalli"
+names1 = "lalli"
+print(names is names1)
+print("loves" in hope)
+
+
+
+#if else, elif
+number6 = 8
+if number6==8:
+    print("corect")
+elif number<10:
+    print("hello")
+else:
+    print("bye")
+
+
+
+#while
+number7 = 0
+while number7<10:
+    print(number7)
+    number7 += 1
+
+
+
+#for
+for anand in range(5):
+    print("laddulovesmaari")
+
+
+#for taking an input
+name8 = int(input())
+print(name8)
+
+
+
